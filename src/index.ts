fd98fca692cd172c3e22604fb8836158d2544760
Leslie-L <leslie.centeno@galileo.edu>
2024-05-08 13:05:57 -0600

    new config

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,9 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './typeDefs';
+import { resolvers } from './resolvers';
 
 
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
-`;
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
-const resolvers = {
-    Query: {
-      books: () => books,
-    },
-  };
 
 const server = new ApolloServer({
     typeDefs,
