bea54f49db5b0a512157afd44535f835a0be9301
Leslie-L <leslie.centeno@galileo.edu>
2024-05-08 13:04:58 -0600

    data for the api

---
+++ src/data.ts
@@ -0,0 +1,152 @@
+export const booksList = [
+    {
+      "id": "9788408209860",
+      "title": "El nombre del viento",
+      "img":"https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/el_nombre_del_viento.jpg?alt=media",
+      "author_id": "9788413140748",
+      "genre": "Fantasía",
+      "year": 2007,
+      "synopsis": "La historia sigue a Kvothe, un joven prodigio en la magia y la música, mientras narra su vida desde su infancia en una troupe de artistas itinerantes hasta convertirse en un legendario mago y músico."
+    },
+    {
+      "id": "9786075274911",
+      "title": "El temor de un hombre sabio",
+      "img":"https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/el_temor_de_un_hombre_sabio.jpg?alt=media",
+      "author_id": "9788413140748",
+      "genre": "Fantasía",
+      "year": 2011,
+      "synopsis": "Segunda parte de la saga del asesino de reyes, donde se narra la continuación de las aventuras de Kvothe."
+    },
+    {
+      "id": "9788420707632",
+      "title": "1984",
+      "img":"https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/1984.jpg?alt=media",
+      "author_id": "9788423327666",
+      "genre": "Ciencia Ficción",
+      "year": 1949,
+      "synopsis": "En un mundo distópico dominado por un régimen totalitario, un hombre llamado Winston Smith lucha por rebelarse contra el control opresivo del Estado."
+    },
+    {
+      "id": "9788499082545",
+      "title": "Rebelión en la granja",
+      "img":"https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/rebelion_en_la_granja.jpg?alt=media",
+      "author_id": "9788423327666",
+      "genre": "Ficción Política",
+      "year": 1945,
+      "synopsis": "Una fábula sobre el poder y la corrupción, donde los animales de una granja expulsan a los humanos y establecen su propio gobierno."
+    },
+    {
+      "id": "9788490325131",
+      "title": "Orgullo y prejuicio",
+      "img":"https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/orgull_y_prejucio.jpg?alt=media",
+      "author_id": "9788491051726",
+      "genre": "Clásico",
+      "year": 1813,
+      "synopsis": "La historia sigue a Elizabeth Bennet mientras navega por el amor y el matrimonio en la sociedad inglesa del siglo XIX."
+    },
+    {
+      "id": "9788494765235",
+      "title": "Sentido y sensibilidad",
+      "img":"https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/sentido_y_sensibilidad.jpg?alt=media",
+      "author_id": "9788491051726",
+      "genre": "Clásico",
+      "year": 1811,
+      "synopsis": "Dos hermanas, Elinor y Marianne Dashwood, deben lidiar con el amor y la pérdida en la Inglaterra del siglo XIX."
+    },
+    {
+      "id": "9788478884451",
+      "title": "Harry Potter y la piedra filosofal",
+      "img":"https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/h_y_p_y_la_piedra_filosofal.jpg?alt=media",
+      "author_id": "978849105112111",
+      "genre": "Fantasía",
+      "year": 1997,
+      "synopsis": "La historia sigue las aventuras de Harry Potter, un niño mago que descubre su verdadera identidad en su undécimo cumpleaños y se une a la escuela de magia Hogwarts, donde se enfrenta a diversos desafíos, incluyendo la búsqueda de la legendaria Piedra Filosofal."
+    },
+    {
+      "id": "9788478884475",
+      "title": "Harry Potter y la cámara secreta",
+      "img":"https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/h_y_p_y_la_camara_secreta.jpg?alt=media",
+      "author_id": "978849105112111",
+      "genre": "Fantasía",
+      "year": 1998,
+      "synopsis": "En su segundo año en Hogwarts, Harry Potter se enfrenta a un misterioso enemigo que ha abierto la legendaria Cámara de los Secretos, liberando una fuerza oscura que amenaza a los estudiantes de sangre mestiza."
+    },
+    {
+      "id": "9788478884499",
+      "title": "Harry Potter y el prisionero de Azkaban",
+      "img":"https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/h_y_p_y_el_prisionero_de_askaban.jpg?alt=media",
+      "author_id": "978849105112111",
+      "genre": "Fantasía",
+      "year": 1999,
+      "synopsis": "En su tercer año en Hogwarts, Harry Potter descubre que un peligroso prisionero, Sirius Black, ha escapado de la prisión de Azkaban y se rumorea que está buscándolo. Mientras tanto, descubre la verdad sobre su pasado y su conexión con el oscuro mago Voldemort."
+    },
+    {
+      "id": "9788478884512",
+      "title": "Harry Potter y el cáliz de fuego",
+      "img":"https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/h_y_p_caliz_de_fuego.jpg?alt=media",
+      "author_id": "978849105112111",
+      "genre": "Fantasía",
+      "year": 2000,
+      "synopsis": "En su cuarto año en Hogwarts, Harry Potter es seleccionado para competir en el Torneo de los Tres Magos, una competición peligrosa que pone a prueba sus habilidades mágicas y lo enfrenta a nuevos desafíos y peligros."
+    },
+    {
+      "id": "9788478884536",
+      "title": "Harry Potter y la Orden del Fénix",
+      "img":"https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/h_y_p_orden_fenix.jpg?alt=media",
+      "author_id": "978849105112111",
+      "genre": "Fantasía",
+      "year": 2003,
+      "synopsis": "En su quinto año en Hogwarts, Harry Potter se enfrenta a la creciente amenaza de Voldemort y sus seguidores, mientras forma parte de un grupo secreto, la Orden del Fénix, que lucha contra las fuerzas oscuras."
+    },
+    {
+      "id": "9788478884574",
+      "title": "Harry Potter y el Príncipe Mestizo",
+      "img": "https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/h_y_p_principe_mestizo.jpg?alt=media",
+      "author_id": "978849105112111",
+      "genre": "Fantasía",
+      "year": 2005,
+      "synopsis": "En la sexta entrega de la serie, Harry Potter regresa a Hogwarts para su sexto año de estudios. Mientras tanto, el mundo mágico se encuentra en peligro creciente debido a los actos malignos de Lord Voldemort y sus seguidores. Harry descubre más sobre el oscuro pasado de Voldemort y se prepara para una inevitable confrontación."
+    },
+    {
+      "id": "9788478884574",
+      "title": "Harry Potter y las Reliquias de la Muerte",
+      "img":"https://firebasestorage.googleapis.com/v0/b/libros-7edb7.appspot.com/o/h_y_p_reliquias_de_la_muerte.jpg?alt=media",
+      "author_id": "978849105112111",
+      "genre": "Fantasía",
+      "year": 2007,
+      "synopsis": "En la conclusión de la serie, Harry Potter emprende una misión para destruir los Horrocruxes restantes de Voldemort y enfrentarse al Señor Oscuro una última vez, en una batalla final entre el bien y el mal."
+    }
+  ]
+  
+export const authorsList =  [
+      {
+        "id": "9788413140748",
+        "name": "Patrick Rothfuss",
+        "birthdate": "1973-06-06",
+        "nationality": "Estadounidense",
+        "bio": "Patrick James Rothfuss es un escritor y profesor estadounidense. Es más conocido por la serie de fantasía \"Crónica del asesino de reyes\", que incluye los libros \"El nombre del viento\", \"El temor de un hombre sabio\" y la tercera entrega aún pendiente de publicación."
+      },
+      {
+        "id": "9788423327666",
+        "name": "George Orwell",
+        "birthdate": "1903-06-25",
+        "deathdate": "1950-01-21",
+        "nationality": "Británico",
+        "bio": "George Orwell, seudónimo de Eric Arthur Blair, fue un novelista, ensayista, crítico y periodista británico, conocido por su obra \"1984\" que introdujo los conceptos del omnipresente y vigilante Gran Hermano y de la notoria habitación 101."
+      },
+      {
+        "id": "9788491051726",
+        "name": "Jane Austen",
+        "birthdate": "1775-12-16",
+        "deathdate": "1817-07-18",
+        "nationality": "Británica",
+        "bio": "Jane Austen fue una destacada novelista británica, conocida principalmente por sus novelas de amor y comedia social. Sus obras más famosas incluyen \"Orgullo y prejuicio\", \"Sentido y sensibilidad\" y \"Emma\"."
+      },
+      {
+        "id": "978849105112111",
+        "name": "J.K. Rowling",
+        "nationality": "Británica",
+        "bio": "Escritora británica, conocida por su serie de novelas 'Harry Potter', que ha vendido millones de copias en todo el mundo.",
+        "birthdate": "1965-07-31"
+      }
+    ]
